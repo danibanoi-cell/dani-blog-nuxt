ed190fe3a0c011887d69cf0120b43c7e302e9db5
Your Namedanibanoi <danibanoi@gmail.com>
2025-12-19 17:08:10 +0100

    chore: ESLint - use vue-eslint-parser and declare Nuxt/Vue auto-imports as globals

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,14 @@ module.exports = {
     node: true,
     es2022: true
   },
-  parser: '@typescript-eslint/parser',
+  // Use vue-eslint-parser to correctly parse Vue SFCs
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    // parser used for the contents of <script> blocks
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2022,
-    sourceType: 'module'
+    sourceType: 'module',
+    extraFileExtensions: ['.vue']
   },
   extends: [
     'eslint:recommended',
@@ -20,7 +24,37 @@ module.exports = {
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
     'no-console': ['warn', { allow: ['error', 'warn'] }],
     'vue/multi-word-component-names': 'off'
-  }
+  },
+  globals: {
+    // Nuxt auto-imported globals
+    useHead: 'readonly',
+    useRoute: 'readonly',
+    useRouter: 'readonly',
+    navigateTo: 'readonly',
+    $fetch: 'readonly',
+    definePageMeta: 'readonly',
+    useI18n: 'readonly',
+    useSwitchLocalePath: 'readonly',
+    useTheme: 'readonly',
+    useState: 'readonly',
+    useRuntimeConfig: 'readonly',
+    // Vue auto-imports
+    ref: 'readonly',
+    computed: 'readonly',
+    onMounted: 'readonly',
+    onBeforeUnmount: 'readonly',
+    // NodeJS types used in some files
+    NodeJS: 'readonly'
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        // Allow script setup single-word components if used
+      }
+    }
+  ]
 }
